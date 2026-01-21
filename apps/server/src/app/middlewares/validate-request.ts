058a2035b0a8e825ad29dc9a9c6df2155f2201ab
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2026-01-21 17:15:07 +0600

    log: add validate request logs

--- apps/server/src/app/middlewares/validate-request.ts
+++ apps/server/src/app/middlewares/validate-request.ts
@@ -1,14 +1,23 @@
+import { logger } from '@app/libs/logger'
 import { catchAsync } from '@repo/shared'
 import type { ZodObject } from 'zod'
 
 export const validateRequest = (schema: ZodObject) => {
   return catchAsync(async (req, res, next) => {
+    logger.debug('Data before validation', {
+      body: req.body,
+      params: req.params,
+      query: req.query,
+      cookies: req.cookies,
+    })
     const { data, success, error } = await schema.safeParseAsync({
       body: req.body,
       params: req.params,
       query: req.query,
       cookies: req.cookies,
     })
+
+    logger.debug('After Validation', data)
 
     if (success) {
       if (data.body) {
